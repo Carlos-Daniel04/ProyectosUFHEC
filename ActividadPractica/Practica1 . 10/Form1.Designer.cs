ea7822fc40f05c2c9ea2bc2a654f45f9a5023044
Carlos-Daniel04 <carlosrijo04@icloud.com>
2026-02-06 19:05:16 -0400

    Practica1 . 10

---
+++ ActividadPractica/Practica1 . 10/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Practica1._10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnclick = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnclick
+            // 
+            this.btnclick.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnclick.Location = new System.Drawing.Point(142, 94);
+            this.btnclick.Name = "btnclick";
+            this.btnclick.Size = new System.Drawing.Size(108, 42);
+            this.btnclick.TabIndex = 0;
+            this.btnclick.Text = "Click";
+            this.btnclick.UseVisualStyleBackColor = true;
+            this.btnclick.Click += new System.EventHandler(this.btnclick_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(397, 213);
+            this.Controls.Add(this.btnclick);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnclick;
+    }
+}
+
---
+++ ActividadPractica/Practica1 . 10/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica1._10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnclick_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hola soy, Carlos daniel y mi matricula es LR-2025-03111");
+        }
+    }
+}
