8aa63304367e13ba30b064d886aa2031264f99c7
Carlos-Daniel04 <carlosrijo04@icloud.com>
2026-02-06 19:43:31 -0400

    Practica1 . 12

---
+++ ActividadPractica/Practica1 . 12/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Practica1._12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnconvertir = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtcelsius = new System.Windows.Forms.TextBox();
+            this.lblresultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnconvertir
+            // 
+            this.btnconvertir.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnconvertir.Location = new System.Drawing.Point(50, 104);
+            this.btnconvertir.Name = "btnconvertir";
+            this.btnconvertir.Size = new System.Drawing.Size(121, 45);
+            this.btnconvertir.TabIndex = 0;
+            this.btnconvertir.Text = "Convertir";
+            this.btnconvertir.UseVisualStyleBackColor = true;
+            this.btnconvertir.Click += new System.EventHandler(this.btnconvertir_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(22, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(240, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Convertir Celsius a Fahrenheit";
+            // 
+            // txtcelsius
+            // 
+            this.txtcelsius.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtcelsius.Location = new System.Drawing.Point(41, 65);
+            this.txtcelsius.Name = "txtcelsius";
+            this.txtcelsius.Size = new System.Drawing.Size(139, 22);
+            this.txtcelsius.TabIndex = 2;
+            // 
+            // lblresultado
+            // 
+            this.lblresultado.AutoSize = true;
+            this.lblresultado.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblresultado.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblresultado.Location = new System.Drawing.Point(177, 117);
+            this.lblresultado.Name = "lblresultado";
+            this.lblresultado.Size = new System.Drawing.Size(0, 19);
+            this.lblresultado.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(386, 220);
+            this.Controls.Add(this.lblresultado);
+            this.Controls.Add(this.txtcelsius);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnconvertir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnconvertir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtcelsius;
+        private System.Windows.Forms.Label lblresultado;
+    }
+}
+
---
+++ ActividadPractica/Practica1 . 12/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica1._12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnconvertir_Click(object sender, EventArgs e)
+        {
+            double celsius;
+            if (double.TryParse(txtcelsius.Text, out celsius))
+            {
+                double fahrenheit = (celsius * 9 / 5) + 32;
+                lblresultado.Text = "Resultado: " + fahrenheit + " °F";
+            }
+            else
+            {
+                MessageBox.Show("Por favor, ingrese un valor numérico válido para Celsius.", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
