b7bf1c4b9fe9268534b76f8a9216fe41b950c9d8
Carlos-Daniel04 <carlosrijo04@icloud.com>
2026-02-09 23:29:17 -0400

    Prtactica1 . 15

---
+++ ActividadPractica/Practica1 . 15/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Practica1._15
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtarchivo = new System.Windows.Forms.TextBox();
+            this.btnAbrir = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.AbrirArchivo = new System.Windows.Forms.OpenFileDialog();
+            this.GuardarArchivo = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // txtarchivo
+            // 
+            this.txtarchivo.Location = new System.Drawing.Point(14, 59);
+            this.txtarchivo.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.txtarchivo.Multiline = true;
+            this.txtarchivo.Name = "txtarchivo";
+            this.txtarchivo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtarchivo.Size = new System.Drawing.Size(240, 33);
+            this.txtarchivo.TabIndex = 0;
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.Location = new System.Drawing.Point(14, 122);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(82, 32);
+            this.btnAbrir.TabIndex = 1;
+            this.btnAbrir.Text = "Abrir";
+            this.btnAbrir.UseVisualStyleBackColor = true;
+            this.btnAbrir.Click += new System.EventHandler(this.btnAbrir_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(133, 122);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(82, 32);
+            this.btnGuardar.TabIndex = 2;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // AbrirArchivo
+            // 
+            this.AbrirArchivo.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(353, 210);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnAbrir);
+            this.Controls.Add(this.txtarchivo);
+            this.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtarchivo;
+        private System.Windows.Forms.Button btnAbrir;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.OpenFileDialog AbrirArchivo;
+        private System.Windows.Forms.SaveFileDialog GuardarArchivo;
+    }
+}
+
---
+++ ActividadPractica/Practica1 . 15/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using file = System.IO.File;
+
+namespace Practica1._15
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAbrir_Click(object sender, EventArgs e)
+        {
+            AbrirArchivo.Filter = "Archivos de texto (*.txt)|*.txt|Todos los archivos (*.*)|*.*";
+            if (AbrirArchivo.ShowDialog() == DialogResult.OK)
+            {
+                txtarchivo.Text = file.ReadAllText(AbrirArchivo.FileName);
+            }
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            GuardarArchivo.Filter = "Archivos de texto (*.txt)|*.txt|Todos los archivos (*.*)|*.*";
+            if (GuardarArchivo.ShowDialog() == DialogResult.OK)
+            {
+                file.WriteAllText(GuardarArchivo.FileName, txtarchivo.Text);
+            }
+        }
+    }
+}
